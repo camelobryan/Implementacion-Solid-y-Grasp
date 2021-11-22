5217a3e7880275039771b37cef64f4ace4aff7c2
camelobryan <94816356+camelobryan@users.noreply.github.com>
2021-11-21 23:25:46 -0500

    Add files via upload

---
+++ Ibero/actividad2.py
@@ -0,0 +1,17 @@
+class Auto:
+    color="Morado"
+    modelo="2021"
+    marca="Lamborghini"
+
+
+Lambo1=Auto()
+Lambo1.color="Naranja"
+print(Lambo1.color,Lambo1.modelo,Lambo1.marca)
+Lambo2=Auto()
+Lambo2.color="Rojo"
+print(Lambo2.color,Lambo2.modelo,Lambo2.marca)
+Ferrari=Auto()
+Ferrari.color="Negro"
+Ferrari.modelo="2008"
+Ferrari.marca="Ferrari"
+print(Ferrari.color,Ferrari.modelo,Ferrari.marca)
